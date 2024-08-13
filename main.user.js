39fa8261e8c2d94a0d7b914e77bfd4acfe03beab
fqdeng <dengfengqi@foxmail.com>
2024-08-13 22:23:49 +0800

    feat: refactor code

--- main.user.js
+++ main.user.js
@@ -89,7 +89,7 @@
             }
         }
 
-        if (position > 0){
+        if (position > 0) {
             for (let i = 0; i < subtitles.length; i++) {
                 let element = subtitles[i]
                 element.style.backgroundColor = '';
@@ -142,8 +142,8 @@
                     subtitleDiv.dataset.idx = i;
 
                     //Prevent double click to select text
-                    subtitleDiv.addEventListener('mousedown', function(e){
-                        if (e.detail > 1){
+                    subtitleDiv.addEventListener('mousedown', function (e) {
+                        if (e.detail > 1) {
                             e.preventDefault();
                         }
                     });
@@ -185,181 +185,92 @@
     }
 
 
-    function renderDraggableDiv() {
+    function loadCss() {
         // Load jQuery UI CSS
         const cssLink = document.createElement('link');
         cssLink.rel = 'stylesheet';
         cssLink.href = 'https://code.jquery.com/ui/1.14.0/themes/base/jquery-ui.css';
         document.head.appendChild(cssLink);
-
-
-        // Wait for the document to be ready
-        $(document).ready(function () {
-            // Get initial position and size from localStorage
-            const initialPositionAndSize = getPositionAndSize();
-            // Create draggable and resizable window elements
-            draggableDiv = document.createElement('div');
-            draggableDiv.id = 'draggableSubtitles';
-            Object.assign(draggableDiv.style, {
-                width: initialPositionAndSize.width,
-                height: initialPositionAndSize.height,
-                zIndex: 1000,
-                padding: '0.5em',
-                backgroundColor: '#f0f0f0',
-                border: '1px solid #ccc',
-                position: 'absolute',
-                left: initialPositionAndSize.left,
-                top: initialPositionAndSize.top,
-            });
-            //init then hide
-            $(draggableDiv).hide();
-
-            const headerDiv = document.createElement('div');
-            headerDiv.className = 'header';
-            Object.assign(headerDiv.style, {
-                cursor: 'move',
-                backgroundColor: '#ccc',
-                padding: '10px',
-                textAlign: 'center',
-                fontWeight: 'bold',
-                fontSize: '14px'
-            });
-            headerDiv.textContent = 'Drag me to change the position';
-            contentDiv.className = 'content';
-            contentDiv.style.padding = '10px';
-
-            windowDiv = document.createElement('div');
-            Object.assign(windowDiv.style, {
-                overflow: 'auto',
-                position: 'absolute',
-                width: '95%',
-                height: '85%',
-            });
-
-            // Assemble the draggable and resizable window
-            windowDiv.appendChild(contentDiv);
-            draggableDiv.appendChild(headerDiv);
-            createSwitchButton();
-            draggableDiv.appendChild(windowDiv);
-
-            const selectContainer = $(`<div id="select-container"></div>`)
-            select = $(`<select id="videoTrackerSelect">
-                <option>Select a video tracker</option>
-                </select>`)
-            selectContainer
-                .css('position', 'absolute')
-                .css('bottom', '-34px')
-                .css('right', '-12px')
-                .css('padding', '5px 10px')
-                .css('z-index', 10000)
-
-            selectContainer.append(select)
-            $(draggableDiv).append(selectContainer);
-
-
-            // Append the draggable and resizable window to the body
-            document.body.appendChild(draggableDiv);
-
-            // Make the window draggable
-            $('#draggableSubtitles').draggable({
-                handle: '.header',
-                stop: function (event, ui) {
-                    const left = ui.position.left + 'px';
-                    const top = ui.position.top + 'px';
-                    const width = $('#draggableSubtitles').width() + 'px';
-                    const height = $('#draggableSubtitles').height() + 'px';
-                    savePositionAndSize(left, top, width, height);
-                }
-            });
-
-            // Make the window resizable
-            $('#draggableSubtitles').resizable({
-                stop: function (event, ui) {
-                    const left = ui.position.left + 'px';
-                    const top = ui.position.top + 'px';
-                    const width = ui.size.width + 'px';
-                    const height = ui.size.height + 'px';
-                    savePositionAndSize(left, top, width, height);
-                }
-            });
-        });
     }
 
+
+    async function renderTrackerSelect(captionTracks) {
+        select.selectmenu({
+            select: function (event, ui) {
+                if (ui.item?.value === -1) {
+                    return;
+                }
+                loadSubtitles(ui.item?.value);
+            }
+        });
+
+        if (!captionTracks || captionTracks.length === 0) {
+            select.empty();
+            const option = $("<option>", {
+                text: "No subtitles",
+                value: -1
+            });
+            select.append(option);
+            select.selectmenu('refresh');
+            console.error('No subtitles found for this video');
+            return;
+        }
+
+    }
+
+    async function loadSubtitlesFromYt(videoId, trackerIdx) {
+        // Fetch video page HTML
+        const response = await fetch(`https://www.youtube.com/watch?v=${videoId}`);
+        const pageHtml = await response.text();
+
+        // Extract player response JSON from the HTML
+        const playerResponseRegex = /ytInitialPlayerResponse\s*=\s*(\{.*?\});/;
+        const playerResponseMatch = pageHtml.match(playerResponseRegex);
+
+        if (!playerResponseMatch) {
+            console.error('Could not extract player response');
+            return;
+        }
+
+        const playerResponse = JSON.parse(playerResponseMatch[1]);
+
+        // Get subtitle tracks
+        const captionTracks = playerResponse.captions?.playerCaptionsTracklistRenderer?.captionTracks;
+        await renderTrackerSelect(captionTracks)
+
+        // Fetch subtitles for the first track
+        let subtitleTrackUrl = captionTracks[0].baseUrl;
+        if (trackerIdx !== null) {
+            subtitleTrackUrl = captionTracks[trackerIdx].baseUrl;
+        } else {
+            select.empty();
+            for (let i = 0; i < captionTracks.length; i++) {
+                const option = $("<option>", {
+                    text: captionTracks[i].name.simpleText,
+                    value: i
+                });
+                select.append(option);
+                select.selectmenu('refresh');
+            }
+        }
+
+        const subtitlesResponse = await fetch(subtitleTrackUrl);
+        const subtitlesXml = await subtitlesResponse.text();
+
+        // Parse XML and extract subtitles
+        const parser = new DOMParser();
+        const subtitlesDoc = parser.parseFromString(subtitlesXml, 'text/xml');
+        return subtitlesDoc.getElementsByTagName('text');
+    }
 
     // Function to load subtitles
     async function renderSubtitles(videoId, trackerIdx) {
         try {
-
             if (!videoId) {
                 console.error('Invalid YouTube URL');
                 return;
             }
-
-            // Fetch video page HTML
-            const response = await fetch(`https://www.youtube.com/watch?v=${videoId}`);
-            const pageHtml = await response.text();
-
-            // Extract player response JSON from the HTML
-            const playerResponseRegex = /ytInitialPlayerResponse\s*=\s*(\{.*?\});/;
-            const playerResponseMatch = pageHtml.match(playerResponseRegex);
-
-            if (!playerResponseMatch) {
-                console.error('Could not extract player response');
-                return;
-            }
-
-            const playerResponse = JSON.parse(playerResponseMatch[1]);
-
-            // Get subtitle tracks
-            const captionTracks = playerResponse.captions?.playerCaptionsTracklistRenderer?.captionTracks;
-
-            select.selectmenu({
-                select: function (event, ui) {
-                    if (ui.item?.value === -1) {
-                        return;
-                    }
-                    loadSubtitles(ui.item?.value);
-                }
-            });
-
-            if (!captionTracks || captionTracks.length === 0) {
-                select.empty();
-                const option = $("<option>", {
-                    text: "No subtitles",
-                    value: -1
-                });
-                select.append(option);
-                select.selectmenu('refresh');
-                console.error('No subtitles found for this video');
-                return;
-            }
-
-
-            // Fetch subtitles for the first track
-            let subtitleTrackUrl = captionTracks[0].baseUrl;
-            if (trackerIdx !== null) {
-
-                subtitleTrackUrl = captionTracks[trackerIdx].baseUrl;
-            } else {
-                select.empty();
-                for (let i = 0; i < captionTracks.length; i++) {
-                    const option = $("<option>", {
-                        text: captionTracks[i].name.simpleText,
-                        value: i
-                    });
-                    select.append(option);
-                    select.selectmenu('refresh');
-                }
-            }
-
-            const subtitlesResponse = await fetch(subtitleTrackUrl);
-            const subtitlesXml = await subtitlesResponse.text();
-
-            // Parse XML and extract subtitles
-            const parser = new DOMParser();
-            const subtitlesDoc = parser.parseFromString(subtitlesXml, 'text/xml');
-            const texts = subtitlesDoc.getElementsByTagName('text');
-
+            const texts = await loadSubtitlesFromYt(videoId, trackerIdx);
             // existing code to fetch and parse subtitles...
             const subtitles = [];
             for (let i = 0; i < texts.length; i++) {
@@ -388,9 +299,99 @@
         }
     };
 
+    function createWindowDiv() {
+        // Get initial position and size from localStorage
+        const initialPositionAndSize = getPositionAndSize();
+        // Create draggable and resizable window elements
+        draggableDiv = document.createElement('div');
+        draggableDiv.id = 'draggableSubtitles';
+        Object.assign(draggableDiv.style, {
+            width: initialPositionAndSize.width,
+            height: initialPositionAndSize.height,
+            zIndex: 1000,
+            padding: '0.5em',
+            backgroundColor: '#f0f0f0',
+            border: '1px solid #ccc',
+            position: 'absolute',
+            left: initialPositionAndSize.left,
+            top: initialPositionAndSize.top,
+        });
+        //init then hide
+        $(draggableDiv).hide();
+
+        const headerDiv = document.createElement('div');
+        headerDiv.className = 'header';
+        Object.assign(headerDiv.style, {
+            cursor: 'move',
+            backgroundColor: '#ccc',
+            padding: '10px',
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fontSize: '14px'
+        });
+        headerDiv.textContent = 'Drag me to change the position';
+        contentDiv.className = 'content';
+        contentDiv.style.padding = '10px';
+
+        windowDiv = document.createElement('div');
+        Object.assign(windowDiv.style, {
+            overflow: 'auto',
+            position: 'absolute',
+            width: '95%',
+            height: '85%',
+        });
+
+        // Assemble the draggable and resizable window
+        windowDiv.appendChild(contentDiv);
+        draggableDiv.appendChild(headerDiv);
+        createSwitchButton();
+        draggableDiv.appendChild(windowDiv);
+
+        const selectContainer = $(`<div id="select-container"></div>`)
+        select = $(`<select id="videoTrackerSelect">
+                <option>Select a video tracker</option>
+                </select>`)
+        selectContainer
+            .css('position', 'absolute')
+            .css('bottom', '-34px')
+            .css('right', '-12px')
+            .css('padding', '5px 10px')
+            .css('z-index', 10000)
+
+        selectContainer.append(select)
+        $(draggableDiv).append(selectContainer);
+
+
+        // Append the draggable and resizable window to the body
+        document.body.appendChild(draggableDiv);
+
+        // Make the window draggable
+        $('#draggableSubtitles').draggable({
+            handle: '.header',
+            stop: function (event, ui) {
+                const left = ui.position.left + 'px';
+                const top = ui.position.top + 'px';
+                const width = $('#draggableSubtitles').width() + 'px';
+                const height = $('#draggableSubtitles').height() + 'px';
+                savePositionAndSize(left, top, width, height);
+            }
+        });
+
+        // Make the window resizable
+        $('#draggableSubtitles').resizable({
+            stop: function (event, ui) {
+                const left = ui.position.left + 'px';
+                const top = ui.position.top + 'px';
+                const width = ui.size.width + 'px';
+                const height = ui.size.height + 'px';
+                savePositionAndSize(left, top, width, height);
+            }
+        });
+    }
 
     function main() {
-        renderDraggableDiv();
+        loadCss();
+        $(document).ready(createWindowDiv)
         //handle YouTube page changed event
         document.addEventListener('fullscreenchange', logFullscreenState);
         document.addEventListener('yt-page-data-updated', onPageChanged);
